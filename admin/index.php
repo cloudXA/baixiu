b3c093b57e0949be0658f5a8b7b7d98f4136d3c2
cloudXA <zming794@gmail.com>
2019-11-22 22:11:52 +0800

    封装查询函数导入输入

--- admin/index.php
+++ admin/index.php
@@ -12,28 +12,46 @@
       header('Location: /admin/login.php');
       exit;
     }
-    // 查询文章总数
-    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    if (!$connection) {
-      die('<h1>Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_errno() . '</h1>');
-    }
-    // 查询posts
-    if ($result = mysqli_query($connection, 'select count(1) from posts')) {
-      $post_count = mysqli_fetch_row($result)[0];
-      mysqli_free_result($result);
-    }
-    // mysqli_close($connection);
+    /**
+     * 执行一个查询语句，返回查询到的数据（关联数组混合索引数组）
+     * @para string $sql 需要执行的查询语句
+     * @return array    查询到的数据 （二位数组）
+     * 
+     */
+    function xiu_query ($sql) {
+      // 建立数据库连接
+      $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+      if (!$connection) {
+        die('<h1>Connection Error (' . mysqli_connect_errno() . ')' .mysqli_connect_errno() . '</h1>');
+      }
 
-    // 查询drafted
-    if ($result = mysqli_query($connection, 'select count(1) from posts where status = \'drafted\'')) {
-      $drafted = mysqli_fetch_row($result)[0];
-      mysqli_free_result($result);
-    }
-    mysqli_close($connection);
+      // 定义结果数据容器，用于装载查询到的数据
+      $data = array();
 
-    
-    
+      // 执行参数中指定的sql语句
+      if ($result = mysqli_query($connection, $sql)) {
+        while ($row = mysqli_fetch_array($result)) {
+          $data[] = $row;
+        }
+
+        // 释放结果集
+        mysqli_free_result($result);
+      }
+      // 关闭数据库
+      mysqli_close($connection);
+      return $data;
+    }
+    // 文章数
+    $post_count = xiu_query('select count(1) from posts')[0][0];
+    // 草稿数
+    $drafted_count = xiu_query('select count(1) from posts where status = \'drafted\'')[0][0];
+    // 分类数组
+    $category_count = xiu_query('select count(1) from categories')[0][0];
+    // 查询评论总数
+    $comment_count = xiu_query('select count(1) from comments')[0][0];
+    // 查询审核评论总数
+    $held_count = xiu_query('select count(1) from comments where status = \'held\'')[0][0];
  
     
 
@@ -77,9 +95,9 @@
               <h3 class="panel-title">站点内容统计：</h3>
             </div>
             <ul class="list-group">
-              <li class="list-group-item"><strong><?php echo isset($post_count) ? $post_count : '查询失败'; ?></strong>篇文章（<strong><?php echo isset($post_count) ? $drafted : '查询失败'; ?></strong>篇草稿）</li>
-              <li class="list-group-item"><strong>6</strong>个分类</li>
-              <li class="list-group-item"><strong>5</strong>条评论（<strong>1</strong>条待审核）</li>
+              <li class="list-group-item"><strong><?php echo $post_count ?></strong>篇文章（<strong><?php echo $drafted_count ?></strong>篇草稿）</li>
+              <li class="list-group-item"><strong><?php echo $category_count ?></strong>个分类</li>
+              <li class="list-group-item"><strong><?php echo $comment_count ?></strong>条评论（<strong><?php echo $held_count ?></strong>条待审核）</li>
             </ul>
           </div>
         </div>
