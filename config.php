cc13d87c0ffedd9c86133719a13533422ac1ccdf
cloudXA <zming794@gmail.com>
2019-11-22 15:04:47 +0800

    项目搭建和解决高亮

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+// 项目用到的配置信息,公共用到的可能会修改的文件
+
+// 数据可主机
+define('DB_HOST', 'localhost');
+// 用户名
+define('DB_USER', 'root');
+// 密码
+define('DB_PASS','laoye7499259');
+// 主机域名
+define('DB_NAME','baixiu-dev');
+
+
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+require_once 'config.php';
+
+?>
+
+<p><?php echo DB_USER; ?></p>
